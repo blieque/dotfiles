f0bb1f6265b61125d97fda329afde22a100c319d
Blieque Mariguan <himself@blieque.co.uk>
2016-10-19 23:05:58 +0100

    fixed and tweaked and stuff

--- diff.sh
+++ diff.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
-echo '.vimrc diff:'
-diff .vimrc ~/.vimrc
-echo '.gvimrc diff:'
-diff .gvimrc ~/.gvimrc
-echo '.zshrc diff:'
-diff .zshrc ~/.zshrc
-echo '.shell-aliases diff:'
-diff .shell-aliases ~/.shell-aliases
+dotfiles=(.vimrc .gvimrc .zshrc .shell-aliases)
+
+for dotfile in "${dotfiles[@]}"; do
+    if [[ -r $dotfile ]]; then
+        echo -e "\e[33m$dotfile diff:\e[0m"
+        diff $dotfile ~/$dotfile
+    fi
+done
+
+if [[ -d ~/.vim/snips/ || -d ~/.vim/UltiSnips/ ]]; then
+    snips_dir=~/.vim/snips
+    [[ -d ~/.vim/UltiSnips ]] && snips_dir=~/.vim/UltiSnips
+
+    snips=(javascript html)
+    for snip in "${snips[@]}"; do
+        snip_file=$snip.snippets
+        echo -e "\e[33m$snip_file diff:\e[0m"
+        diff snips/$snip_file $snips_dir/$snip_file
+    done
+fi
