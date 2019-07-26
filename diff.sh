ae74555f9e96a4fb541012aaae860c30aa92cdc6
Blieque Mariguan <himself@blieque.co.uk>
2019-07-26 18:11:42 +0100

    feat: Improve scripts
    - Use `tput` for colours in `diff.sh`.
    - Use two-space indentation.
    - Add `.bashrc` to `diff.sh` file list.

--- diff.sh
+++ diff.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
-dotfiles=(.vimrc .gvimrc .zshrc .shell-aliases)
+dotfiles=(.vimrc .gvimrc .bashrc .zshrc .shell-aliases)
 
 for dotfile in "${dotfiles[@]}"; do
-    if [[ -r $dotfile ]]; then
-        echo -e "\e[33m$dotfile diff:\e[0m"
-        diff $dotfile ~/$dotfile
-    fi
+  if [[ -r $dotfile ]]; then
+    tput setaf 3 && tput bold && echo "$dotfile diff:" && tput sgr0
+    diff $dotfile ~/$dotfile
+    echo
+  fi
 done
 
 if [[ -d ~/.vim/snips/ || -d ~/.vim/UltiSnips/ ]]; then
-    snips_dir=~/.vim/snips
-    [[ -d ~/.vim/UltiSnips ]] && snips_dir=~/.vim/UltiSnips
+  snips_dir=~/.vim/snips
+  [[ -d ~/.vim/UltiSnips ]] && snips_dir=~/.vim/UltiSnips
 
-    snips=(javascript html)
-    for snip in "${snips[@]}"; do
-        snip_file=$snip.snippets
-        echo -e "\e[33m$snip_file diff:\e[0m"
-        diff snips/$snip_file $snips_dir/$snip_file
-    done
+  snips=(javascript html)
+  for snip in "${snips[@]}"; do
+    snip_file=$snip.snippets
+    tput setaf 3 && tput bold && echo "$snip_file diff:" && tput sgr0
+    diff snips/$snip_file $snips_dir/$snip_file
+    echo
+  done
 fi
--- install.sh
+++ install.sh
@@ -6,13 +6,13 @@ cp -i .gvimrc ~
 cp -i .zshrc ~
 cp -i .shell-aliases ~
 if [[ "$(uname -s)" == "Darwin" ]]; then
-    cp -i .mac-shell-rc ~
+  cp -i .mac-shell-rc ~
 fi
 
 if [[ -d ~/.vim/snips/ ]]; then
-    cp -i snips/* ~/.vim/snips/
+  cp -i snips/* ~/.vim/snips/
 elif [[ -d ~/.vim/UltiSnips/ ]]; then
-    cp -i snips/* ~/.vim/UltiSnips/
+  cp -i snips/* ~/.vim/UltiSnips/
 else
-    mkdir -p ~/.vim/snips/ && cp -i snips/* ~/.vim/snips/
+  mkdir -p ~/.vim/snips/ && cp -i snips/* ~/.vim/snips/
 fi
