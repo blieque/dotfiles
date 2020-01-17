112fab50bdf6c305ee43ca441cd02d8fbba57fd2
Blieque Mariguan <himself@blieque.co.uk>
2020-01-17 17:06:34 +0000

    feat: Update powerline-shell theme
    - Update "hollow" powerline-shell theme with more colours.

--- themes/hollow.py
+++ themes/hollow.py
@@ -1,21 +1,24 @@
 """
 
 Hollow Theme
-Blieque Mariguan (@blieque, GitHub and Twitter)
+Blieque Mariguan
 
-For use with powerline-shell (milkbikis/powerline-shell).
+For use with powerline-shell (b-ryan/powerline-shell).
 MIT licensed.
 
 """
+
+from powerline_shell.themes.default import DefaultColor
 
 class Color(DefaultColor):
 
     HOME_SPECIAL_DISPLAY = True
     HOME_BG = 33        # solarised blue
     HOME_FG = 230       # cream
-    PATH_BG = 235       # dark grey
-    PATH_FG = 246       # light-ish grey
-    CWD_FG = 251        # light grey
+
+    PATH_BG = 236       # dark grey
+    PATH_FG = 244       # light-ish grey
+    CWD_FG = 250        # light grey
     SEPARATOR_FG = 240  # little-lighter-than-dark-grey grey
 
     READONLY_BG = 124   # strong red
@@ -24,17 +27,32 @@ class Color(DefaultColor):
     SSH_BG = 56         # blue-ish purple
     SSH_FG = 230        # cream
 
-    REPO_CLEAN_BG = 61  # lilac-ish
-    REPO_CLEAN_FG = 230 # cream
-    REPO_DIRTY_BG = 125 # hot pink-ish purple
-    REPO_DIRTY_FG = 230 # cream
+    GIT_CLEAN_BG = 61  # lilac-ish
+    GIT_CLEAN_FG = 230 # cream
+    GIT_DIRTY_BG = 125 # hot pink-ish purple
+    GIT_DIRTY_FG = 230 # cream
 
-    CMD_PASSED_BG = 234 # dark grey
+    GIT_AHEAD_BG = 61
+    GIT_AHEAD_FG = 230
+    GIT_BEHIND_BG = 61
+    GIT_BEHIND_FG = 230
+    GIT_STAGED_BG = 61
+    GIT_STAGED_FG = 230
+    GIT_NOTSTAGED_BG = 61
+    GIT_NOTSTAGED_FG = 230
+    GIT_UNTRACKED_BG = 61
+    GIT_UNTRACKED_FG = 230
+    GIT_CONFLICTED_BG = 61
+    GIT_CONFLICTED_FG = 230
+    GIT_STASH_BG = 61
+    GIT_STASH_FG = 230
+
+    CMD_PASSED_BG = 235 # dark grey
     CMD_PASSED_FG = 251 # light-ish grey
-    CMD_FAILED_BG = 197 # pink
-    CMD_FAILED_FG = 230 # cream
+    CMD_FAILED_BG = 235 # pink
+    CMD_FAILED_FG = 197 # cream
 
-    # START UNUSED
+    # Unused
 
     USERNAME_FG = 250
     USERNAME_BG = 240
@@ -51,5 +69,3 @@ class Color(DefaultColor):
 
     JOBS_FG = 33
     JOBS_BG = 238
-
-    # END UNUSED
